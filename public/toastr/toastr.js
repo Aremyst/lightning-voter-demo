b4a2ae6c81d32b76f0a547f3e573af35fb5a1bb2
Dmitriy Naimark <dmitrynaimark@gmail.com>
2018-07-24 10:44:31 +0300

    Add my comments to 'toastr' wrapper

--- public/toastr/toastr.js
+++ public/toastr/toastr.js
@@ -1,8 +1,11 @@
+// Service wrapper around global 'toastr' variable.
 (function() {
+  // Create 'toastr' module.
   var toastrModule = angular.module('toastr', []);
   
+  // Config toastr using global 'toastr'.
   toastr.options.timeOut = 1000;
   
+  // Create value(simple service), which returns global 'toastr'.
   toastrModule.value('toastr', toastr);
-  
-}())
+}());
